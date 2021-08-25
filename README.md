5ac32cd008ed51dc3e087517dba93fdadb179d67
convict-git <official.mr.convict@gmail.com>
2021-08-25 08:43:04 +0530

    Finally made it working

--- README.md
+++ README.md
@@ -1,12 +1,40 @@
 # Fullstack GraphQL
+
 > Learn how to use GraphQL with Node and React
 
 This course comes with some [slides](https://docs.google.com/presentation/d/1IrGA4PtUEZPVDTBg5_WCMmUapElbFBgLwfSBAp8ft1g/edit?usp=sharing)
 
-
 ## What you'll need
-* Node version >= 6
+
+- Node version >= 6
 
 ## Solutions
+
 The solution branch has the completed course fo reference. There is no one way to finish this course.
 `git checkout solution`
+
+To make this work
+
+1.  ```bash
+    rm -rf yarn.lock package-lock.json node_modules
+    ```
+
+2.  in `.babelrc`
+
+    ```diff
+    + "presets": ["@babel/env", "@babel/react"],
+    -  "presets": ["env", "react"],
+    ```
+
+3.  in `package.json`
+
+    ```diff
+    + "resolutions": {
+    +   "@babel/preset-env": "7.13.8"
+    + },
+    ```
+
+4.  ```bash
+    yarn install
+    yarn app
+    ```
--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,33 @@
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import gql from 'graphql-tag'
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
 
 /**
  * Create a new apollo client and export as default
  */
+// const link = new HttpLink({ uri: "http://localhost:4000/" });
+const link = new HttpLink({ uri: "https://rickandmortyapi.com/graphql" });
+const cache = new InMemoryCache(); // good to go with default settings as well
+
+const client = new ApolloClient({ link, cache });
+
+const query = gql`
+  {
+    characters {
+      results {
+        name
+        id
+        __typename
+      }
+    }
+  }
+`;
+
+console.log("hello");
+client
+  .query({ query })
+  .then((data) => console.log(data))
+  .catch((error) => console.error(error));
+
+export default client;
