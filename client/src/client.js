105d944162000a4bb169360cb62acc4b2d29c154
convict-git <official.mr.convict@gmail.com>
2021-08-30 13:52:17 +0530

    Frontend master basic exercise

--- client/src/client.js
+++ client/src/client.js
@@ -2,32 +2,41 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 import gql from "graphql-tag";
+import { sleep } from "./utils/delay";
+import { setContext } from "apollo-link-context";
+import { ApolloLink } from "apollo-link";
 
 /**
  * Create a new apollo client and export as default
  */
+// const link = new HttpLink({ uri: "https://rickandmortyapi.com/graphql" });
 // const link = new HttpLink({ uri: "http://localhost:4000/" });
-const link = new HttpLink({ uri: "https://rickandmortyapi.com/graphql" });
-const cache = new InMemoryCache(); // good to go with default settings as well
 
-const client = new ApolloClient({ link, cache });
+const delay = setContext((request) => sleep(2000));
+const httpLink = new HttpLink({ uri: "http://localhost:4000/" });
+const link = ApolloLink.from([delay, httpLink]); // composition of Apollo links
 
-const query = gql`
-  {
-    characters {
-      results {
-        name
-        id
-        __typename
-      }
-    }
+// client side schemas
+const typeDefs = gql`
+  extend type User {
+    age: Int
+  }
+  extend type Pet {
+    vaccinated: Boolean!
   }
 `;
 
-console.log("hello");
-client
-  .query({ query })
-  .then((data) => console.log(data))
-  .catch((error) => console.error(error));
+const resolvers = {
+  User: {
+    age: () => 35,
+  },
+  Pet: {
+    vaccinated: () => true,
+  },
+};
+
+const cache = new InMemoryCache(); // good to go with default settings as well
+
+const client = new ApolloClient({ link, cache, typeDefs, resolvers });
 
 export default client;
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -1,21 +1,90 @@
-import React, {useState} from 'react'
-import gql from 'graphql-tag'
-import { useQuery, useMutation } from '@apollo/react-hooks'
-import PetsList from '../components/PetsList'
-import NewPetModal from '../components/NewPetModal'
-import Loader from '../components/Loader'
+import React, { useState } from "react";
+import gql from "graphql-tag";
+import { useQuery, useMutation } from "@apollo/react-hooks";
+import PetsList from "../components/PetsList";
+import NewPetModal from "../components/NewPetModal";
+import Loader from "../components/Loader";
 
-
-export default function Pets () {
-  const [modal, setModal] = useState(false)
-
-
-  const onSubmit = input => {
-    setModal(false)
+const PETS_FIELDS = gql`
+  fragment PetsField on Pet {
+    id
+    name
+    type
+    img
+    owner {
+      age @client
+    }
+    vaccinated @client
   }
-  
+`;
+
+const GET_PETS = gql`
+  query AllPets {
+    pets {
+      ...PetsField
+    }
+  }
+  ${PETS_FIELDS}
+`;
+
+const CREATE_PET = gql`
+  mutation CreatePet($newPet: NewPetInput!) {
+    addPet(input: $newPet) {
+      ...PetsField
+    }
+  }
+  ${PETS_FIELDS}
+`;
+
+export default function Pets() {
+  const [modal, setModal] = useState(false);
+  const pets = useQuery(GET_PETS);
+
+  // const [createPet, newPet] = useMutation(CREATE_PET); // without updating the cache
+
+  const [createPet, newPet] = useMutation(CREATE_PET, {
+    update(cache, { data: { addPet } }) {
+      const { pets } = cache.readQuery({ query: GET_PETS });
+
+      console.log(pets);
+      cache.writeQuery({
+        query: GET_PETS,
+        data: { pets: [addPet, ...pets] },
+      });
+    },
+    // optimisitic updates can be handled here as well
+  });
+
+  // if (pets.loading || newPet.loading) {
+  if (pets.loading) {
+    // don't load on newPet now (optimistically update)
+    return <Loader />;
+  }
+
+  if (pets.error || newPet.error) {
+    return <div>Error!!!</div>;
+  }
+
+  const onSubmit = (input) => {
+    setModal(false);
+    createPet({
+      variables: { newPet: input },
+      optimisticResponse: {
+        addPet: {
+          id: toString(Math.floor(Math.random() * 100000)),
+          name: input.name,
+          type: input.type,
+          img: "https://via.placeholder.com/300",
+          __typename: "Pet",
+        },
+      },
+    });
+  };
+
+  console.log(pets.data);
+
   if (modal) {
-    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />
+    return <NewPetModal onSubmit={onSubmit} onCancel={() => setModal(false)} />;
   }
 
   return (
@@ -32,8 +101,8 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={pets.data.pets} />
       </section>
     </div>
-  )
+  );
 }
